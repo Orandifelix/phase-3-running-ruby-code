ef12ae538cbe6f842299d5221f6f8d177040e831
Orandifelix <125347734+Orandifelix@users.noreply.github.com>
2023-06-22 03:50:34 +0300

    Done

--- app.rb
+++ app.rb
@@ -4,15 +4,18 @@
 #     { id: 4, hello: "Learner" }]
 
 # p [1,2,3]
-# puts "Hello World!"
-# print "Pass this test, please."
-# p [1,2,3]
+
 
 # def add
     
 # end
 # # 1 + 1
 # puts add + add
+
+
+puts "Hello World!"
+print "Pass this test, please."
+p [1,2,3]
 
 def add num1, num2
     puts num1
